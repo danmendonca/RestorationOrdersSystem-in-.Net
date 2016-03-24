5376896d9e10269a79542154a51cba9651a429aa
rodolfoaar <rodolfo.raar@gmail.com>
2016-03-24 23:11:29 +0000

    BarKitchenClient UI

--- BarKitchenClient/BarKitchenClient.cs
+++ BarKitchenClient/BarKitchenClient.cs
@@ -4,19 +4,28 @@ using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
+using System.Runtime.Remoting;
+
 namespace BarKitchenClient
 {
     static class BarKitchenClient
     {
-        /// <summary>
-        /// The main entry point for the application.
-        /// </summary>
         [STAThread]
         static void Main()
         {
+
+            RemotingConfiguration.Configure("BarKitchenClient.exe.config", false);
+
+            SingleServer ss = new SingleServer();
+            
+            
+            
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new BarKitchenWindow());
         }
+        
     }
+
+    
 }
--- BarKitchenClient/BarKitchenWindow.cs
+++ BarKitchenClient/BarKitchenWindow.cs
@@ -12,9 +12,37 @@ namespace BarKitchenClient
 {
     public partial class BarKitchenWindow : Form
     {
+
+        List<RequestLine> requests;
+
         public BarKitchenWindow()
         {
             InitializeComponent();
         }
+
+        private void BarKitchenWindow_Load(object sender, EventArgs e)
+        {
+            requests = new List<RequestLine>();
+            ShowRequests();
+        }
+
+        private void ShowRequests()
+        {
+            ListViewItem item1 = new ListViewItem("Something");
+            item1.SubItems.Add("SubItem1a");
+            item1.SubItems.Add("SubItem1b");
+
+
+            ListViewItem item2 = new ListViewItem("Something2");
+            item2.SubItems.Add("SubItem2a");
+            item2.SubItems.Add("SubItem2b");
+
+            ListViewItem item3 = new ListViewItem("Something3");
+            item3.SubItems.Add("SubItem3a");
+            item3.SubItems.Add("SubItem3b");
+
+            listView1.Items.AddRange(new ListViewItem[] { item1, item2, item3 });
+        }
+
     }
 }
